ebabd70eab72c0fbfadfa47524d43f7841b3fd93
Elektro <13082613+ElektroStudios@users.noreply.github.com>
2024-02-03 04:57:41 +0100

    Update Loader.cmd
    v1.1

--- Loader.cmd
+++ Loader.cmd
@@ -1,17 +1,44 @@
-@ECHO OFF & TITLE ps3-disc-dumper loader
+@ECHO OFF & TITLE ps3-disc-dumper loader v1.1
 
-REM Use this batch-script file to run the "ps3-disc-dumper.exe" program without the UAC restriction.
+REM These variables let you specify a custom file name and directory path.
 
-SET "execName=ps3-disc-dumper"
+SET "fileName=ps3-disc-dumper.exe"
+SET "dirPath=%~dp0"
 
-IF NOT EXIST "%~dp0%execName%.exe" (
-    ECHO:Cannot find file: "%~dp0%execName%.exe"
+REM We check whether the file exists.
+
+SET "fullPath=%dirPath%\%fileName%"
+IF NOT EXIST "%fullPath%" (
+    ECHO:Cannot find file: "%fullPath%"
     ECHO:This program will close now...
     PAUSE
     EXIT 1
 )
 
-START /B "%execName%" "%~dp0%execName%.exe" ^
-         "/IUnderstandThatRunningSoftwareAsAdministratorIsDangerousAndNotRecommendedForAnyone"
+REM Uncomment all the next lines starting with "::" if you wat to set custom default program settings:
 
-EXIT 0
+:: MKDIR "%LocalAppData%\ps3-disc-dumper" 2>NUL
+:: MKDIR "%~dp0output" 2>NUL
+:: MKDIR "%~dp0ird" 2>NUL
+:: 
+:: (
+::   ECHO:{
+::   ECHO:  "OutputDir"^: "output",
+::   ECHO:  "IrdDir"^: "ird",
+::   ECHO:  "DumpNameTemplate"^: "%%title%% [%%product_code%%]",
+::   ECHO:  "ShowDetails"^: true,
+::   ECHO:  "EnableTransparency"^: false,
+::   ECHO:  "PreferSystemAccent"^: false,
+::   ECHO:  "StayOnTop"^: false,
+::   ECHO:  "CopyBdmv"^: true,
+::   ECHO:  "CopyPs3Update"^: false
+::   ECHO:}
+:: )>"%LocalAppData%\ps3-disc-dumper\settings.json"
+
+REM Finally, we run the program with the special command-line switch.
+
+(START /B "%fileName%" "%fullPath%" ^
+         "/IUnderstandThatRunningSoftwareAsAdministratorIsDangerousAndNotRecommendedForAnyone"
+) && (
+    EXIT 0
+)
